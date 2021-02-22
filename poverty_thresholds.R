efd0440e4ecc4e35f5df9558de15a486b85d13f4
Jose Andres Moya Porras <jose.91cr@gmail.com>
2021-02-22 15:44:06 -0600

    Adding new chart

--- poverty_thresholds.R
+++ poverty_thresholds.R
@@ -14,4 +14,11 @@ poverty_threshold_chart_generator <- function(region, df){
     hc_plotOptions(areaspline = list(opacity = 0.5))
 }
 
+poverty_threshold_regions_chart_generator <- function(threshold, df){
+  hchart(filter(df,Threshold==threshold), "spline", 
+         hcaes(x=Year, y=Percentage, group=Entity)) %>% 
+    hc_colors(viridisLite::viridis(7)) %>%
+    hc_legend(enabled = TRUE,align = "left", verticalAlign = "top") %>%
+    hc_plotOptions(areaspline = list(opacity = 0.5))
+}
 
